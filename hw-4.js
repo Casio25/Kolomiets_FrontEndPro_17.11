f34219d568bbd30df3e9ff8aafe502c43a602a57
casio25 <mishakolomiets355@gmail.com>
2022-11-20 20:20:50 +0200

    hw-4

---
+++ hw-4.js
@@ -0,0 +1,47 @@
+const age = prompt("Введіть свій вік: ");
+
+if (age == undefined || age == "") {
+    alert("ви не ввели свій вік");
+}else if(isNaN(age)){
+    alert(`${age} is not a number`);
+}
+
+const city = prompt("Введіть своє місто: ");
+switch (city) {
+    case "Київ":
+        alert("Ти живеш у столиці України");
+        break;
+    case "Вашингтон":
+        alert("Ти живеш у столиці США");
+        break;
+    case "Лондон":
+        alert("Ти живеш у столиці Великобританії");
+        break;
+    case null:
+    case "":
+        alert("Ви не ввели назву міста");
+        break;
+    default:
+        alert(`Ти живеш у місці ${city}`);
+        break;
+}
+
+const sport = prompt("Введіть свій удюблений вид спорту: ");
+switch (sport) {
+    case "Футбол":
+        alert("Круто! Хочеш стати Мессі?");
+        break;
+    case "Баскетбол":
+        alert("Круто! Хочеш стати Майклом Джорданом?");
+        break;
+    case "Бокс":
+        alert("Круто! Хочеш стати Тайсоном?");
+        break;
+    case null:
+    case "":
+        alert("Ви не ввели назву свого улюбленого виду спорту");
+        break;
+    default:
+        alert("Круто! Спорт це здорово");
+        break;
+}
