725277be8560bb60894e7b3b6f8e196bf8232c72
casio25 <mishakolomiets355@gmail.com>
2023-05-10 19:26:02 +0300

    hamburger test

---
+++ hw-44/hw-44.js
@@ -0,0 +1,46 @@
+class Hamburger {
+    constructor(price, calories) {
+        this.price = price;
+        this.calories = calories;
+        this.toppings = [];
+    }
+
+    addTopping(topping) {
+        this.toppings.push(topping);
+        this.price += topping.price;
+        this.calories += topping.calories;
+    }
+    calculatePrice(){
+        return this.price
+    }
+    calculateCalories(){
+        return this.calories 
+    }
+
+    static addMayo(hamburger) {
+        hamburger.addTopping({ price: 10, calories: 50 });
+    }
+
+    static addKetchup(hamburger) {
+        hamburger.addTopping({ price: 5, calories: 20 });
+    }
+}
+
+class SmallHamburger extends Hamburger {
+    constructor() {
+        super(50, 20);
+    }
+}
+
+class BigHamburger extends Hamburger {
+    constructor() {
+        super(100, 40);
+    }
+}
+
+let burger = new BigHamburger();
+Hamburger.addKetchup(burger);
+Hamburger.addMayo(burger);
+
+
+
